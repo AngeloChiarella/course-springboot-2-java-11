4b64f590039fe9d039c594e7142aa542108b0af9
Angelo Chiarella <chiarella.gaetano@gmail.com>
2022-02-07 13:22:27 -0300

    Project created

---
+++ src/main/java/com/educandoweb/coursejpa/CoursejpaApplication.java
@@ -0,0 +1,15 @@
+package com.educandoweb.coursejpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursejpaApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(CoursejpaApplication.class, args);
+	}
+
+}
