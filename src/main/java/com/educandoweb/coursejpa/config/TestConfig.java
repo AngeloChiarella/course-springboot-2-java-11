e64805a7b9db0a480357b1779bbca7a95a777812
Angelo Chiarella <chiarella.gaetano@gmail.com>
2022-02-08 13:51:03 -0300

    Payment, one-to-one association

--- src/main/java/com/educandoweb/coursejpa/config/TestConfig.java
+++ src/main/java/com/educandoweb/coursejpa/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.educandoweb.coursejpa.entities.Category;
 import com.educandoweb.coursejpa.entities.Order;
 import com.educandoweb.coursejpa.entities.OrderItem;
+import com.educandoweb.coursejpa.entities.Payment;
 import com.educandoweb.coursejpa.entities.Product;
 import com.educandoweb.coursejpa.entities.User;
 import com.educandoweb.coursejpa.entities.enums.OrderStatus;
@@ -82,5 +83,10 @@ public class TestConfig implements CommandLineRunner //CommandLineRunner void ru
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);//associar pedido 1 com pagamento 1
+		orderRepository.save(o1);//salvando obj dependente OneToOne
+		
 	}
 }
--- src/main/java/com/educandoweb/coursejpa/entities/Order.java
+++ src/main/java/com/educandoweb/coursejpa/entities/Order.java
@@ -5,6 +5,7 @@ import java.time.Instant;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -12,6 +13,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import com.educandoweb.coursejpa.entities.enums.OrderStatus;
@@ -38,6 +40,9 @@ public class Order implements Serializable
 
 	@OneToMany(mappedBy = "id.order") // OrderItem tem id(PK), e o id tem o pedido
 	private Set<OrderItem> items = new HashSet<>();
+	
+	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)//Mapeando as entidades para ter o mesmo Id
+	private Payment payment;
 
 	public Order()
 	{
@@ -96,11 +101,21 @@ public class Order implements Serializable
 		this.client = client;
 	}
 
+	public Payment getPayment()
+	{
+		return payment;
+	}
+
+	public void setPayment(Payment payment)
+	{
+		this.payment = payment;
+	}
+
 	public Set<OrderItem> getItems()
 	{
 		return items;
 	}
-
+	
 	@Override
 	public int hashCode()
 	{
